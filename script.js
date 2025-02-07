8ccac1a5a83716f1f74f9d639266fd03e7146d38
kexkali001 <eduardo1275rodrigues@gmail.com>
2025-02-06 22:12:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.getElementById('btnSaudacao').addEventListener('click', function() {
-    document.getElementById('saudacao').innerText = 'Olá, amante dos pets! Obrigado por visitar nosso site!';
-});
 
-document.getElementById('btnWhatsApp').addEventListener('click', function() {
-    const mensagem = encodeURIComponent("Olá, gostaria de fazer um pedido!");
-    const numeroWhatsApp = "5516992320582";
-    const urlWhatsApp = https://wa.me/${numeroWhatsApp}?text=${mensagem};
-    window.open(urlWhatsApp, '_blank');
-});
